b2d86c1ce352537bab335515dee534f18369e157
Bianca <contatobmuniz@gmail.com>
2025-05-31 17:35:39 -0300

    feat: get user_email

--- api.py
+++ api.py
@@ -12,6 +12,20 @@ def read_hello_world():
 
 ## inserir usuário no banco
 
-@app.post("/users")
+#@app.post("/users")
+#def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#    return services.create_user(db=db, user=user)
+
+@app.get("/users/{user_email}")
+def read_user_by_email(user_email: str, db:Session=Depends(get_db)):
+    user = services.get_user_by_email(db=db, email=user_email)
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    return user
+
+@app.post("/users", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = services.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="E-mail já cadastrado")
     return services.create_user(db=db, user=user)
--- services.py
+++ services.py
@@ -11,3 +11,7 @@ def create_user(db: Session, user: UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+
+def get_user_by_email(db: Session, email:str):
+    return db.query(User).filter(User.email==email).first()
